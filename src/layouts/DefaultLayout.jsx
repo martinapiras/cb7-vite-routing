1b760d631741782e5bc1c82c742d716743c57179
martinapiras <131855775+martinapiras@users.noreply.github.com>
2023-07-11 20:12:54 +0200

    11-07-2023: added default layout

---
+++ src/layouts/DefaultLayout.jsx
@@ -0,0 +1,19 @@
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import { Outlet } from "react-router-dom";
+import styles from "./DefaultLayout.module.scss";
+
+const DefaultLayout = ({ children }) => {
+  return (
+    <div className={styles.DefaultLayout}>
+      <Header />
+      <div className={styles.content}>
+        {children}
+        <Outlet />
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default DefaultLayout;
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
+import DefaultLayout from "./layouts/";
 import App from "./App.jsx";
 import LocationsList from "./routes/Locations";
 import Locations from "./routes/Locations/Locations.jsx";
@@ -8,30 +14,40 @@ import About from "./routes/About";
 import Contacts from "./routes/Contacts/";
 import "./index.css";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        path: "/locations",
-        element: <LocationsList />,
-      },
-      {
-        path: "/locations/:id",
-        element: <Locations />,
-      },
-      {
-        path: "/about",
-        element: <About />,
-      },
-      {
-        path: "/contacts",
-        element: <Contacts />,
-      },
-    ],
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <App />,
+//   },
+//   {
+//     path: "/locations",
+//     element: <LocationsList />,
+//   },
+//   {
+//     path: "/locations/:id",
+//     element: <Locations />,
+//   },
+//   {
+//     path: "/about",
+//     element: <About />,
+//   },
+//   {
+//     path: "/contacts",
+//     element: <Contacts />,
+//   },
+// ]);
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route element={<DefaultLayout />}>
+      <Route path="/" element={<App />} />
+      <Route path="/locations" element={<LocationsList />} />
+      <Route path="/locations/:id" element={<Locations />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contacts" element={<Contacts />} />
+    </Route>
+  )
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
